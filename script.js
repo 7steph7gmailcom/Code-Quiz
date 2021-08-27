85977d588417d4b4ef7b1499b79d132907f44fab
stephaniecjohansson <7steph7@gmail.com>
2021-08-27 14:02:03 -0600

    Q/A

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
-function myFunction() {
-  alert("Hello");
-}
+var startButton = document.getElementById("start button");
+
+var questionsContainerEl = document.getElementById("questions_container");
+
+var questionEl = document.getElementById("questions");
+
+var 
+
+
+
+
+
+var questions = [
+  {
+    question: "What do you call an animal that has more than 5 toes/fingers on thier paws?",
+    answers: [
+      { A: "Multidigital" },
+      { B: "Polydactyl" },
+      { C: "Twotoomany" },
+    ],
+    Answer: "B, Polydactyl",
+  },
+  {
+    question: "Which of the following traits are true about cats?",
+    answers: [
+      { A: "They are heat seekers"},
+      { B: "Cats will sleep on average 16 -20 hours per day"},
+      { C: "Felines are "true carnivores" - they need protein in order to live" },
+      { D: "All the above" },
+    ],
+    Answer: "D, All the above are true!",
+  },
+  {
+    question: "What is the purpose of whiskers on a dog or cat?",
+    answers: [
+      { A: "They are ornimental/they serve no real purpose" },
+      { B: "Whiskers can be used as weapons when the animal is feeling threatened" },
+      { C: "They help a dog or cat identify and interact with thier surrounding environment" },
+    ],
+    Answer: "C, they are extra sensory equipment for the animal",
+  },
+  {
+    question: "Has your dog ever had Kennel Cough?, What is Kennel Cough?",
+    answers: [
+      { A: "A highly contagious disease caused by being in close proximity to other infected dogs" },
+      { B: "Seasonal allergies making your dog cough that occur with too much time spent outdoors" },
+      { C: "A disease caused by either a virus or bacteria. With it's main telltale symptom as a persistant goose or honking cough" },
+    ],
+    Answer: "Jupiter",
+  },
+  {
+    question: "Is it better to have a cat or dog as a family member?",
+    answers: [
+      { A: "Only cats because dogs slobber too much" },
+      { B: "Depends on your personality and that of the animal" },
+      { C: "Definately dogs, cats are too temperamental" },
+    ],
+    Answer: "B, They are both wonderful!!!",
+  },
+];
+
