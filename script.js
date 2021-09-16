a76fe1edf49087507a6f58efebed0dc6c471a2c4
stephaniecjohansson <7steph7@gmail.com>
2021-09-16 17:13:31 -0600

    startQ's

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ const questions = [
 ];
 
 // Set constant references to elements
-
 var questionIndex = 0;
 var timeLeft = document.getElementById("timeLeft");
 var initialInput = document.getElementById("initialInput");
@@ -36,6 +35,21 @@ var questionDiv = document.getElementById("questionDiv");
 var startDiv = document.getElementById("start");
 var timer = document.getElementById("timer");
 var timesUp = document.getElementById("timesUp");
+var questionTitle = document.getElementById("questionTitle");
+var answerCheck = document.getElementById("answerCheck");
+var startQuizBtn = document.getElementById("start-quiz-button");
+
+
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
+choiceA.addEventListener("click", chooseA);
+choiceB.addEventListener("click", chooseB);
+choiceC.addEventListener("click", chooseC);
+choiceD.addEventListener("click", chooseD);
+
+
 
 // WHEN I click the start button, timer starts
 var totalTime = 91;
@@ -63,4 +77,72 @@ function newQuiz() {
 
   showQuiz();
 };
+
+// console.log(questions[questionIndex].question);
+// console.log(questions[questionIndex].choices);
+
+startQuizBtn.addEventListener("click", newQuiz);
+
+function showQuiz() {
+  nextQuestion();
+}
+
+function nextQuestion() {
+  questionTitle.textContent = questions[questionIndex].question;
+  choiceA.textContent = questions[questionIndex].choices[0];
+  choiceB.textContent = questions[questionIndex].choices[1];
+  choiceC.textContent = questions[questionIndex].choices[2];
+  choiceD.textContent = questions[questionIndex].choices[3];
+}
+
+console.log(nextQuestion);
+
+// Show user if correct or wrong
+function checkAnswer(answer) {
+
+  var lineBreak = document.getElementById("lineBreak");
+  lineBreak.style.display = "block";
+  answerCheck.style.display = "block";
+
+  if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+      // correct answer = 1 score to final score
+      correctAns++;
+      console.log(correctAns);
+      answerCheck.textContent = "Correct!";
+  } else {
+      // wrong answer = deduct 10 second from timer
+      totalTime -= 10;
+      timeLeft.textContent = totalTime;
+      answerCheck.textContent = "Incorrect! The correct answer is: " + questions[questionIndex].answer;
+  }
+
+  questionIndex++;
+  // Iterate through questions 
+  if (questionIndex < questions.length) {
+      nextQuestion();
+  } else {
+      // if no more questions, gameOver =>
+      gameOver();
+  }
+}
+
+// After all questions are answered or timer reaches 0, "Game over"
+function gameOver() {
+  summary.style.display = "block";
+  questionDiv.style.display = "none";
+  startDiv.style.display = "none";
+  timer.style.display = "none";
+  timesUp.style.display = "block";
+
+  // show final score
+  finalScore.textContent = correctAns;
+}
+
+function chooseA() { checkAnswer(0); }
+
+function chooseB() { checkAnswer(1); }
+
+function chooseC() { checkAnswer(2); }
+
+function chooseD() { checkAnswer(3); }
 
